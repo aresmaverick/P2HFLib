bf8a46fad735f2b906158de85d300232e595fb83
Frederico de Oliveira <fredericoliveira@gmail.com>
2016-05-13 19:14:27 -0300

    Sigo trabalhando no teste do WebConnector.

--- src/test/java/br/com/p2hf/web/connect/TestWebConnector.java
+++ src/test/java/br/com/p2hf/web/connect/TestWebConnector.java
@@ -9,6 +9,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import br.com.p2hf.connect.Connector;
+import br.com.p2hf.web.connect.model.Request;
 import br.com.p2hf.web.server.StandAloneWebService;
 
 public class TestWebConnector {
@@ -16,6 +18,8 @@ public class TestWebConnector {
 	private static final String BASE_URL = "http://localhost:9090";
 	private static final String PACAKGE = "br.com.p2hf.web";
 	StandAloneWebService saws;
+	
+	URI uriChamada;
 	
 	@Before
 	public void init() {
@@ -38,7 +42,16 @@ public class TestWebConnector {
 	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
-	}
+		try {
+			uriChamada = new URI(BASE_URL + "/myresource");
+			
+			Connector webConnector = new WebConnector();
+			Request request = new Request(uriChamada);
 
+			webConnector.send
+			
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
 }
