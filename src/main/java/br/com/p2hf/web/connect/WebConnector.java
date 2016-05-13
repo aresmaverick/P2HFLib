def1d323e8ee94b3f893ef6f032ca792fb8002bd
Frederico de Oliveira <fredericoliveira@gmail.com>
2016-05-13 06:17:25 -0300

    Inicio da classe do WebConnector.

---
+++ src/main/java/br/com/p2hf/web/connect/WebConnector.java
@@ -0,0 +1,14 @@
+package br.com.p2hf.web.connect;
+
+import br.com.p2hf.connect.Connector;
+import br.com.p2hf.web.connect.model.Request;
+import br.com.p2hf.web.connect.model.Response;
+
+public class WebConnector implements Connector {
+
+	public Response send(Request request) {
+		
+		
+		return new Response();
+	}
+}
