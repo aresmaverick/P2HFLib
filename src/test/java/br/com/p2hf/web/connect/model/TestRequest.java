5f1e5d37563c245c28181817153967eff72b21d6
Frederico de Oliveira <fredericoliveira@gmail.com>
2016-05-13 06:08:20 -0300

    Adicionei as classes de testes mas ainda não escrevi nada ainda.

---
+++ src/test/java/br/com/p2hf/web/connect/model/TestRequest.java
@@ -0,0 +1,14 @@
+package br.com.p2hf.web.connect.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRequest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/br/com/p2hf/web/connect/model/TestResponse.java
@@ -0,0 +1,14 @@
+package br.com.p2hf.web.connect.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestResponse {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
