9228af2cae4c86b86cbc24f30e020098f7f96d0c
Frederico de Oliveira <fredericoliveira@gmail.com>
2016-05-13 06:14:25 -0300

    Criação da Interface Connector. Acho que vou mudar esse cara de lugar.

---
+++ src/main/java/br/com/p2hf/web/connect/Connector.java
@@ -0,0 +1,11 @@
+package br.com.p2hf.web.connect;
+
+public interface Connector {
+
+	public static String POST = "POST";
+	public static String GET = "GET";
+	
+	public static String USER_AGENT = "Mozilla/5.0";
+	
+	
+}
